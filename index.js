5438bfb28a08642fe767dc6bc76614180fbb648a
pranab56 <pronabhalder53@gmail.com>
2022-07-12 17:47:42 +0600

    order product

--- index.js
+++ index.js
@@ -27,11 +27,15 @@ function verifyJWT(req,res,next){
 
 const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fowny.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 async function run(){
     try{
         await client.connect();
         const productCollection = client.db("fashion").collection("shop");
-        const profileCollection = client.db("fashion").collection("profile");
+        const usersCollection = client.db("fashion").collection("users");
+        const orderCollection = client.db("fashion").collection("order");
+        
         app.get('/',(req,res)=>{
             res.send('data')
         })
@@ -44,15 +48,60 @@ async function run(){
         app.get('/productdetails/:id',async(req,res)=>{
             const id=req.params.id;
             const query={_id:ObjectId(id)}
-            const result=await productCollection.findOne(query);
-            res.send(result)
+            const result= await productCollection.findOne(query);
+            res.send(result);
         })
+      
+        // all create user login and update
 
-        app.post('/profile',async(req,res)=>{
+        app.post('/users',async(req,res)=>{
             const query=req.body;
-            const updateProfile=await profileCollection.insertOne(query);
+            const updateProfile=await usersCollection.insertOne(query);
             res.send(updateProfile)
         })
+
+        app.put('users/:email',async(req,res)=>{
+            const email=req.params.email;
+            const query=req.body;
+            const filter={email:email};
+            const options = { upsert: true };
+            const updateDoc={
+                $set: query,
+            };
+            const result=await usersCollection.updateOne(filter,updateDoc,options);
+            res.send(result)
+        })
+        
+        app.get('/users/:email', async(req,res)=>{
+            const email=req.params.email;
+            const query={email:email};
+            const result=await usersCollection.findOne(query);
+            res.send(result)
+          })
+
+       
+
+        app.put('/users/:email',async(req,res)=>{
+            const email=req.params.email;
+            const user=req.body;
+            const filter={email:email};
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+              };
+              const result=await usersCollection.updateOne(filter,updateDoc,options)
+              const token=jwt.sign({email:email},process.env.TOKEN,{ expiresIn: '1h' })
+              res.send({result,token})
+        })
+
+
+        app.post('/order',async(req,res)=>{
+            const order=req.body;
+            const result=await orderCollection.insertOne(order);
+            res.send(result)
+        })
+      
+
     
     }
     finally{
